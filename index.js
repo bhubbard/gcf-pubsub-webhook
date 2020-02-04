ff66aff6756db736a0a80084ed16ff0263cb82d7
Brandon Hubbard <bhubbard@users.noreply.github.com>
2020-02-04 09:41:47 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const {PubSub} = require('@google-cloud/pubsub');
+const pubsub = new PubSub();
+
+const pubsub_topic = process.env.PUBSUB_TOPIC;
+
+// Publish to Pub/Sub.
+exports.publish = async (req, res) => {
+
+  console.log( "Publishing message to topic ${'" +  pubsub_topic + "'}");
+
+  // References an existing topic
+  const topic = pubsub.topic( pubsub_topic );
+
+  const messageObject = {
+    data: {
+      message: req.body,
+    },
+  };
+  const messageBuffer = Buffer.from(JSON.stringify(messageObject), 'utf8');
+  
+  console.log( JSON.stringify(req.body) );
+
+  // Publishes a message
+  try {
+    await topic.publish(messageBuffer);
+    res.status(200).send('Webhook Successful.');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+    return Promise.reject(err);
+  }
+};
+
